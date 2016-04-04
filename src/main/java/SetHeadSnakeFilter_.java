52918578ec8e63053b43dd3fba2a7b706a71415d
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-04-04 14:03:50 +0100

    Creating structures

---
+++ src/main/java/SetHeadSnakeFilter_.java
@@ -0,0 +1,22 @@
+import uk.ac.warwick.wsbc.QuimP.plugin.utils.QWindowBuilder;
+
+/**
+ * @file SetHeadSnakeFilter_.java
+ * @date 4 Apr 2016
+ */
+
+/**
+ * @author p.baniukiewicz
+ * @date 4 Apr 2016
+ *
+ */
+public class SetHeadSnakeFilter_ extends QWindowBuilder {
+
+    /**
+     * 
+     */
+    public SetHeadSnakeFilter_() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
