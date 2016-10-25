83a300fd13308cfc852f46d1e9ed689684c72b23
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2016-10-25 09:36:40 +0100

    #245 Added options for reading version for plugins from pom

--- src/main/java/quimp/plugin/SetHeadSnakeFilter_.java
+++ src/main/java/quimp/plugin/SetHeadSnakeFilter_.java
@@ -21,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.warwick.wsbc.QuimP.Node;
+import uk.ac.warwick.wsbc.QuimP.PropertyReader;
 import uk.ac.warwick.wsbc.QuimP.Snake;
 import uk.ac.warwick.wsbc.QuimP.ViewUpdater;
 import uk.ac.warwick.wsbc.QuimP.geom.ExtendedVector2d;
@@ -117,7 +118,12 @@ public class SetHeadSnakeFilter_ extends QWindowBuilder implements IQuimpBOASnak
 
     @Override
     public String getVersion() {
-        return "1.0.1";
+        String trimmedClassName = getClass().getSimpleName();
+        trimmedClassName = trimmedClassName.substring(0, trimmedClassName.length() - 1); // no _
+        // _ at the end of class does not appears in final jar name, we need it to
+        // distinguish between plugins
+        return PropertyReader.readProperty(getClass(), trimmedClassName,
+                "quimp/plugin/plugin.properties", "internalVersion");
     }
 
     /**
