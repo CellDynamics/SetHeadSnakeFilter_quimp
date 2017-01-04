c0223f6cb9774fe875573e8a9776ba01ab6c873b
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-01-04 15:05:48 +0000

    #315 Switched to scijava vecmath

--- src/main/java/quimp/plugin/SetHeadSnakeFilter_.java
+++ src/main/java/quimp/plugin/SetHeadSnakeFilter_.java
@@ -14,9 +14,9 @@ import java.awt.geom.Rectangle2D;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.vecmath.Point2d;
-import javax.vecmath.Vector2d;
 
+import org.scijava.vecmath.Point2d;
+import org.scijava.vecmath.Vector2d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
