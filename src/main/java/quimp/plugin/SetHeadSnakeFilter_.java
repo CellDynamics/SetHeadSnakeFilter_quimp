024bd666b2bc2d517e01653a0131376a5a48e2a7
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-05-25 13:47:05 +0100

    Updated tests and reflected changes in QuimP API Fixes #1

--- src/main/java/quimp/plugin/SetHeadSnakeFilter_.java
+++ src/main/java/quimp/plugin/SetHeadSnakeFilter_.java
@@ -52,230 +52,230 @@ import uk.ac.warwick.wsbc.quimp.plugin.utils.QWindowBuilder;
 public class SetHeadSnakeFilter_ extends QWindowBuilder implements IQuimpBOASnakeFilter,
         IQuimpPluginSynchro, ChangeListener, ActionListener, ItemListener {
 
-    static final Logger LOGGER = LoggerFactory.getLogger(SetHeadSnakeFilter_.class.getName());
-    private ParamList uiDefinition; /*!< Definition of UI */
-    protected ViewUpdater qcontext; /*!< remember QuimP context to recalculate and update its view*/
-    private String method; /*!< method of choosing head node as entry in Choice selector */
-    private Snake snake; /*!< reference to input data */
+  static final Logger LOGGER = LoggerFactory.getLogger(SetHeadSnakeFilter_.class.getName());
+  private ParamList uiDefinition; /*!< Definition of UI */
+  protected ViewUpdater qcontext; /*!< remember QuimP context to recalculate and update its view*/
+  private String method; /*!< method of choosing head node as entry in Choice selector */
+  private Snake snake; /*!< reference to input data */
 
-    /**
-     * Construct GUI window
-     */
-    public SetHeadSnakeFilter_() {
-        // create UI using QWindowBuilder
-        uiDefinition = new ParamList(); // will hold ui definitions
-        // configure window, names of UI elements are also names of variables
-        // exported/imported by set/getPluginConfig
-        uiDefinition.put("name", "SetHeadSnakeFilter"); // name of win
-        uiDefinition.put("method", "choice, minX, nearestCentroid, minXY");
-        uiDefinition.put("help",
-                "Short description:\nminX - set head to point with smallest X coordinate"
-                        + "\nnearestCentroid - set head to point which is closest to centroid point"
-                        + "\nminXY - set head to point which is closest to corner of bounding box");
-        buildWindow(uiDefinition); // construct ui (not shown yet)
+  /**
+   * Construct GUI window
+   */
+  public SetHeadSnakeFilter_() {
+    // create UI using QWindowBuilder
+    uiDefinition = new ParamList(); // will hold ui definitions
+    // configure window, names of UI elements are also names of variables
+    // exported/imported by set/getPluginConfig
+    uiDefinition.put("name", "SetHeadSnakeFilter"); // name of win
+    uiDefinition.put("method", "choice, minX, nearestCentroid, minXY");
+    uiDefinition.put("help",
+            "Short description:\nminX - set head to point with smallest X coordinate"
+                    + "\nnearestCentroid - set head to point which is closest to centroid point"
+                    + "\nminXY - set head to point which is closest to corner of bounding box");
+    buildWindow(uiDefinition); // construct ui (not shown yet)
+  }
+
+  @Override
+  public int setup() {
+    return DOES_SNAKES + MODIFY_INPUT;
+  }
+
+  /**
+   * Configure plugin and overrides default values.
+   * 
+   * Supported keys:
+   * <ol>
+   * <li>\c method - method of choosing head node
+   * </ol>
+   * 
+   * @param par configuration as pairs <key,val>. Keys are defined by plugin creator and plugin
+   *        caller do not modify them.
+   * @throws QuimpPluginException on wrong parameters list or wrong parameter conversion
+   * @see uk.ac.warwick.wsbc.quimp.plugin.IQuimpPlugin#setPluginConfig(ParamList)
+   */
+  @Override
+  public void setPluginConfig(ParamList par) throws QuimpPluginException {
+    try {
+      method = par.getStringValue("method");
+      setValues(par); // copy incoming parameters to UI
+    } catch (Exception e) {
+      // we should never hit this exception as parameters are not touched by caller they are
+      // only passed to configuration saver and restored from it
+      throw new QuimpPluginException("Wrong input argument->" + e.getMessage(), e);
     }
 
-    @Override
-    public int setup() {
-        return DOES_SNAKES + MODIFY_INPUT;
-    }
+  }
 
-    /**
-     * Configure plugin and overrides default values.
-     * 
-     * Supported keys:
-     * <ol>
-     * <li>\c method - method of choosing head node
-     * </ol>
-     * 
-     * @param par configuration as pairs <key,val>. Keys are defined by plugin creator and plugin
-     *        caller do not modify them.
-     * @throws QuimpPluginException on wrong parameters list or wrong parameter conversion
-     * @see uk.ac.warwick.wsbc.quimp.plugin.IQuimpPlugin#setPluginConfig(ParamList)
-     */
-    @Override
-    public void setPluginConfig(ParamList par) throws QuimpPluginException {
-        try {
-            method = par.getStringValue("method");
-            setValues(par); // copy incoming parameters to UI
-        } catch (Exception e) {
-            // we should never hit this exception as parameters are not touched by caller they are
-            // only passed to configuration saver and restored from it
-            throw new QuimpPluginException("Wrong input argument->" + e.getMessage(), e);
-        }
+  @Override
+  public ParamList getPluginConfig() {
+    return getValues();
+  }
 
-    }
+  @Override
+  public int showUi(boolean val) {
+    toggleWindow(val);
+    return 0;
+  }
 
-    @Override
-    public ParamList getPluginConfig() {
-        return getValues();
-    }
+  @Override
+  public String getVersion() {
+    String trimmedClassName = getClass().getSimpleName();
+    trimmedClassName = trimmedClassName.substring(0, trimmedClassName.length() - 1); // no _
+    // _ at the end of class does not appears in final jar name, we need it to
+    // distinguish between plugins
+    return PropertyReader.readProperty(getClass(), trimmedClassName,
+            "quimp/plugin/plugin.properties", "internalVersion");
+  }
 
-    @Override
-    public int showUi(boolean val) {
-        toggleWindow(val);
-        return 0;
-    }
-
-    @Override
-    public String getVersion() {
-        String trimmedClassName = getClass().getSimpleName();
-        trimmedClassName = trimmedClassName.substring(0, trimmedClassName.length() - 1); // no _
-        // _ at the end of class does not appears in final jar name, we need it to
-        // distinguish between plugins
-        return PropertyReader.readProperty(getClass(), trimmedClassName,
-                "quimp/plugin/plugin.properties", "internalVersion");
-    }
-
-    /**
-     * Main plugin runner
-     * 
-     */
-    @Override
-    public Snake runPlugin() throws QuimpPluginException {
-        // TODO Clean this method moving cases to separate functions
-        method = getStringFromUI("method");
-        LOGGER.debug(String.format("Run plugin with params: method %s", method));
-        int pos = 1;
-        Node n = snake.getHead();
-        switch (method) {
-            case "minX": {
-                // change tp asPolygon, and get bounds
-                double minX = n.getX();
-                n = n.getNext();
-                do {
-                    if (n.getX() < minX) {
-                        minX = n.getX();
-                        pos = n.getTrackNum();
-                    }
-                    LOGGER.trace("node: " + n.toString());
-                    n = n.getNext();
-                } while (!n.isHead());
-                // go to pos node
-                snake.setNewHead(pos);
-                break;
-            }
-            case "nearestCentroid": {
-                ExtendedVector2d c = snake.getCentroid();
-                double mindist = Double.MAX_VALUE;
-                do {
-                    ExtendedVector2d p = new ExtendedVector2d(n.getX(), n.getY()); // vec from node
-                    p.sub(c); // vector node - centroid
-                    double len = p.length(); // distance between node and centroid
-                    if (len < mindist) {
-                        mindist = len;
-                        pos = n.getTrackNum();
-                    }
-                    LOGGER.trace("Distance: " + len + " between " + n.toString());
-                    n = n.getNext();
-                } while (!n.isHead());
-                // go to pos node
-                snake.setNewHead(pos);
-                break;
-            }
-            case "minXY": {
-                pos = findNearestToBoundingBox(snake);
-                // go to pos node
-                snake.setNewHead(pos);
-                break;
-            }
-            default:
-                throw new QuimpPluginException("Method not supported. check config data");
-        }
-        return snake;
-    }
-
-    /**
-     * Return point which is nearest to lower left point of bounding box.
-     * 
-     * @param s Snake to be analyzed
-     * @return Index of Snake Node which is closest to considered point. Nodes are numbered from 1
-     */
-    private int findNearestToBoundingBox(Snake s) {
-        Rectangle2D.Double bounds = s.getDoubleBounds();
-        LOGGER.debug("Rectangle pos: " + bounds.getX() + " " + bounds.getY());
-        Point2d p0 = new Point2d(bounds.getX(), bounds.getY());
-        // calculate lengths
-        Node n = s.getHead();
-        int pos = 1;
-        double len;
-        double minlen = Double.MAX_VALUE;
+  /**
+   * Main plugin runner
+   * 
+   */
+  @Override
+  public Snake runPlugin() throws QuimpPluginException {
+    // TODO Clean this method moving cases to separate functions
+    method = getStringFromUI("method");
+    LOGGER.debug(String.format("Run plugin with params: method %s", method));
+    int pos = 1;
+    Node n = snake.getHead();
+    switch (method) {
+      case "minX": {
+        // change tp asPolygon, and get bounds
+        double minX = n.getX();
+        n = n.getNext();
         do {
-            Vector2d v = new Vector2d(n.getX(), n.getY());
-            v.sub(p0);
-            len = v.length();
-            if (len < minlen) {
-                minlen = len;
-                pos = n.getTrackNum();
-            }
-            LOGGER.trace("Distance: " + len + " between " + n.toString());
-            n = n.getNext();
+          if (n.getX() < minX) {
+            minX = n.getX();
+            pos = n.getTrackNum();
+          }
+          LOGGER.trace("node: " + n.toString());
+          n = n.getNext();
         } while (!n.isHead());
-        return pos;
+        // go to pos node
+        snake.setHead(pos);
+        break;
+      }
+      case "nearestCentroid": {
+        ExtendedVector2d c = snake.getCentroid();
+        double mindist = Double.MAX_VALUE;
+        do {
+          ExtendedVector2d p = new ExtendedVector2d(n.getX(), n.getY()); // vec from node
+          p.sub(c); // vector node - centroid
+          double len = p.length(); // distance between node and centroid
+          if (len < mindist) {
+            mindist = len;
+            pos = n.getTrackNum();
+          }
+          LOGGER.trace("Distance: " + len + " between " + n.toString());
+          n = n.getNext();
+        } while (!n.isHead());
+        // go to pos node
+        snake.setHead(pos);
+        break;
+      }
+      case "minXY": {
+        pos = findNearestToBoundingBox(snake);
+        // go to pos node
+        snake.setHead(pos);
+        break;
+      }
+      default:
+        throw new QuimpPluginException("Method not supported. check config data");
+    }
+    return snake;
+  }
+
+  /**
+   * Return point which is nearest to lower left point of bounding box.
+   * 
+   * @param s Snake to be analyzed
+   * @return Index of Snake Node which is closest to considered point. Nodes are numbered from 1
+   */
+  private int findNearestToBoundingBox(Snake s) {
+    Rectangle2D.Double bounds = s.getDoubleBounds();
+    LOGGER.debug("Rectangle pos: " + bounds.getX() + " " + bounds.getY());
+    Point2d p0 = new Point2d(bounds.getX(), bounds.getY());
+    // calculate lengths
+    Node n = s.getHead();
+    int pos = 1;
+    double len;
+    double minlen = Double.MAX_VALUE;
+    do {
+      Vector2d v = new Vector2d(n.getX(), n.getY());
+      v.sub(p0);
+      len = v.length();
+      if (len < minlen) {
+        minlen = len;
+        pos = n.getTrackNum();
+      }
+      LOGGER.trace("Distance: " + len + " between " + n.toString());
+      n = n.getNext();
+    } while (!n.isHead());
+    return pos;
+  }
+
+  @Override
+  public void attachData(Snake data) {
+    if (data == null)
+      return;
+    snake = data;
+  }
+
+  @Override
+  public void attachContext(ViewUpdater b) {
+    qcontext = b;
+  }
+
+  /**
+   * Override window builder to add listeners to GUI
+   */
+  @Override
+  public void buildWindow(final ParamList def) {
+    super.buildWindow(def); // window must be built first
+    // attach listener to selected ui
+    ((Choice) ui.get("method")).addItemListener(this);
+    applyB.addActionListener(this); // attach listener to apply button
+  }
+
+  /**
+   * Update view on any change in GUI
+   */
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    Object b = e.getSource();
+    if (b == applyB) { // pressed apply, copy ui data to plugin
+      qcontext.updateView();
+      LOGGER.debug("actionPerformed called");
+    }
+  }
+
+  /**
+   * Update view on any change in GUI
+   */
+  @Override
+  public void stateChanged(ChangeEvent ce) {
+    if (isWindowVisible() == true) {
+      qcontext.updateView();
+      LOGGER.debug("stateChanged called");
     }
 
-    @Override
-    public void attachData(Snake data) {
-        if (data == null)
-            return;
-        snake = data;
-    }
+  }
 
-    @Override
-    public void attachContext(ViewUpdater b) {
-        qcontext = b;
+  /**
+   * Update view on any change in GUI
+   */
+  @Override
+  public void itemStateChanged(ItemEvent e) {
+    if (isWindowVisible() == true) {
+      qcontext.updateView();
+      LOGGER.debug("stateChanged called");
     }
+  }
 
-    /**
-     * Override window builder to add listeners to GUI
-     */
-    @Override
-    public void buildWindow(final ParamList def) {
-        super.buildWindow(def); // window must be built first
-        // attach listener to selected ui
-        ((Choice) ui.get("method")).addItemListener(this);
-        applyB.addActionListener(this); // attach listener to apply button
-    }
-
-    /**
-     * Update view on any change in GUI
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        Object b = e.getSource();
-        if (b == applyB) { // pressed apply, copy ui data to plugin
-            qcontext.updateView();
-            LOGGER.debug("actionPerformed called");
-        }
-    }
-
-    /**
-     * Update view on any change in GUI
-     */
-    @Override
-    public void stateChanged(ChangeEvent ce) {
-        if (isWindowVisible() == true) {
-            qcontext.updateView();
-            LOGGER.debug("stateChanged called");
-        }
-
-    }
-
-    /**
-     * Update view on any change in GUI
-     */
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        if (isWindowVisible() == true) {
-            qcontext.updateView();
-            LOGGER.debug("stateChanged called");
-        }
-    }
-
-    @Override
-    public String about() {
-        return "Select head node in outline\nAuthor: Piotr Baniukiewicz\n"
-                + "mail: p.baniukiewicz@warwick.ac.uk";
-    }
+  @Override
+  public String about() {
+    return "Select head node in outline\nAuthor: Piotr Baniukiewicz\n"
+            + "mail: p.baniukiewicz@warwick.ac.uk";
+  }
 
 }
--- src/test/java/quimp/plugin/SetHeadSnakeFilter_Test.java
+++ src/test/java/quimp/plugin/SetHeadSnakeFilter_Test.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.assertEquals;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,107 +19,127 @@ import uk.ac.warwick.wsbc.quimp.BOAState;
 import uk.ac.warwick.wsbc.quimp.BOA_;
 import uk.ac.warwick.wsbc.quimp.Node;
 import uk.ac.warwick.wsbc.quimp.Snake;
-import uk.ac.warwick.wsbc.quimp.plugin.utils.DataLoader;
-
-/**
- * @file SetHeadSnakeFilter_Test.java
- * @date 10 Apr 2016
- */
+import uk.ac.warwick.wsbc.quimp.utils.test.DataLoader;
 
 /**
  * @author p.baniukiewicz
- * @date 10 Apr 2016
  *
  */
+@RunWith(MockitoJUnitRunner.class)
 public class SetHeadSnakeFilter_Test {
-    static final Logger LOGGER = LoggerFactory.getLogger(SetHeadSnakeFilter_Test.class.getName());
+  static final Logger LOGGER = LoggerFactory.getLogger(SetHeadSnakeFilter_Test.class.getName());
 
-    /**
-     * Accessor to private fields
-     * 
-     * @param name Name of private method
-     * @param obj Reference to object
-     * @param param Array of parameters if any
-     * @param paramtype Array of classes of \c param
-     * @throws SecurityException
-     * @throws NoSuchMethodException
-     * @throws InvocationTargetException
-     * @throws IllegalArgumentException
-     * @throws IllegalAccessException
-     * @return Private method
-     */
-    static Object accessPrivate(String name, SetHeadSnakeFilter_ obj, Object[] param,
-            Class<?>[] paramtype) throws NoSuchMethodException, SecurityException,
-            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        Method prv = obj.getClass().getDeclaredMethod(name, paramtype);
-        prv.setAccessible(true);
-        return prv.invoke(obj, param);
+  /**
+   * Accessor to private fields
+   * 
+   * @param name Name of private method
+   * @param obj Reference to object
+   * @param param Array of parameters if any
+   * @param paramtype Array of classes of \c param
+   * @throws SecurityException
+   * @throws NoSuchMethodException
+   * @throws InvocationTargetException
+   * @throws IllegalArgumentException
+   * @throws IllegalAccessException
+   * @return Private method
+   */
+  static Object accessPrivate(String name, SetHeadSnakeFilter_ obj, Object[] param,
+          Class<?>[] paramtype) throws NoSuchMethodException, SecurityException,
+          IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    Method prv = obj.getClass().getDeclaredMethod(name, paramtype);
+    prv.setAccessible(true);
+    return prv.invoke(obj, param);
+  }
+
+  private DataLoader d1, d2, d3;
+  private SetHeadSnakeFilter_ testobj;
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    testobj = new SetHeadSnakeFilter_();
+    d1 = new DataLoader("src/test/resources/test1.txt");
+    d2 = new DataLoader("src/test/resources/test2.txt");
+    d3 = new DataLoader("src/test/resources/test3.txt");
+    BOA_.qState = new BOAState();
+  }
+
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void testRunPlugin() throws Exception {
+    SetHeadSnakeFilter_ tmpo = Mockito.spy(new SetHeadSnakeFilter_());
+    Mockito.when(tmpo.getStringFromUI("method")).thenReturn("minX");
+    PolygonRoi pr = new PolygonRoi(d3.getFloatPolygon(), Roi.POLYGON);
+    Snake s = new Snake(pr, 1);
+    tmpo.attachData(s);
+    tmpo.runPlugin();
+    Node h = s.getHead();
+    assertEquals(3, h.getX(), 1e-5);
+    assertEquals(6, h.getY(), 1e-5);
+
+    Mockito.when(tmpo.getStringFromUI("method")).thenReturn("minXY");
+    s = new Snake(pr, 1);
+    tmpo.attachData(s);
+    tmpo.runPlugin();
+    h = s.getHead();
+    assertEquals(3.5, h.getX(), 1e-5);
+    assertEquals(2.5, h.getY(), 1e-5);
+
+    Mockito.when(tmpo.getStringFromUI("method")).thenReturn("nearestCentroid");
+    s = new Snake(pr, 1);
+    tmpo.attachData(s);
+    tmpo.runPlugin();
+    h = s.getHead();
+    assertEquals(3.0, h.getX(), 1e-5);
+    assertEquals(3.0, h.getY(), 1e-5);
+  }
+
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void testFindNearestToBoundingBox() throws Exception {
+    PolygonRoi pr = new PolygonRoi(d1.getFloatPolygon(), Roi.POLYGON);
+    Snake s = new Snake(pr, 1);
+    int ret = (int) accessPrivate("findNearestToBoundingBox", testobj, new Object[] { s },
+            new Class[] { Snake.class });
+    assertEquals(1, ret);
+  }
+
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void testFindNearestToBoundingBox_1() throws Exception {
+    PolygonRoi pr = new PolygonRoi(d2.getFloatPolygon(), Roi.POLYGON);
+    LOGGER.debug("testFindNearestToBoundingBox_1" + d2.toString());
+    Snake s = new Snake(pr, 1);
+    // dirty hack - move head if it was set to last node due to removing fake head in Snake
+    // constructor
+    Node n = s.getHead();
+    if (n.getX() == 4) {
+      s.setHead(n.getNext().getTrackNum());
     }
+    int ret = (int) accessPrivate("findNearestToBoundingBox", testobj, new Object[] { s },
+            new Class[] { Snake.class });
+    assertEquals(4, ret);
+  }
 
-    private DataLoader d1, d2, d3;
-    private SetHeadSnakeFilter_ testobj;
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        testobj = new SetHeadSnakeFilter_();
-        d1 = new DataLoader("src/test/resources/test1.txt");
-        d2 = new DataLoader("src/test/resources/test2.txt");
-        d3 = new DataLoader("src/test/resources/test3.txt");
-        BOA_.qState = new BOAState();
-    }
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
-     * @throws Exception
-     */
-    @Test
-    public void testFindNearestToBoundingBox() throws Exception {
-        PolygonRoi pr = new PolygonRoi(d1.getFloatPolygon(), Roi.POLYGON);
-        Snake s = new Snake(pr, 1);
-        int ret = (int) accessPrivate("findNearestToBoundingBox", testobj, new Object[] { s },
-                new Class[] { Snake.class });
-        assertEquals(1, ret);
-    }
-
-    /**
-     * @throws Exception
-     */
-    @Test
-    public void testFindNearestToBoundingBox_1() throws Exception {
-        PolygonRoi pr = new PolygonRoi(d2.getFloatPolygon(), Roi.POLYGON);
-        LOGGER.debug("testFindNearestToBoundingBox_1" + d2.toString());
-        Snake s = new Snake(pr, 1);
-        // dirty hack - move head if it was set to last node due to removing fake head in Snake
-        // constructor
-        Node n = s.getHead();
-        if (n.getX() == 4) {
-            s.setNewHead(n.getNext().getTrackNum());
-        }
-        int ret = (int) accessPrivate("findNearestToBoundingBox", testobj, new Object[] { s },
-                new Class[] { Snake.class });
-        assertEquals(4, ret);
-    }
-
-    /**
-     * @throws Exception
-     */
-    @Test
-    public void testFindNearestToBoundingBox_2() throws Exception {
-        PolygonRoi pr = new PolygonRoi(d3.getFloatPolygon(), Roi.POLYGON);
-        LOGGER.debug(d3.toString());
-        Snake s = new Snake(pr, 1);
-        int ret = (int) accessPrivate("findNearestToBoundingBox", testobj, new Object[] { s },
-                new Class[] { Snake.class });
-        assertEquals(5, ret);
-    }
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void testFindNearestToBoundingBox_2() throws Exception {
+    PolygonRoi pr = new PolygonRoi(d3.getFloatPolygon(), Roi.POLYGON);
+    LOGGER.debug(d3.toString());
+    Snake s = new Snake(pr, 1);
+    int ret = (int) accessPrivate("findNearestToBoundingBox", testobj, new Object[] { s },
+            new Class[] { Snake.class });
+    assertEquals(5, ret);
+  }
 
 }
