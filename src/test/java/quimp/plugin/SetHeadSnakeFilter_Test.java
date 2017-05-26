9abc3af7f1d4c39900f50788bb4beae871022f78
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-05-26 09:34:48 +0100

    Test can be run from jar.

--- src/test/java/quimp/plugin/SetHeadSnakeFilter_Test.java
+++ src/test/java/quimp/plugin/SetHeadSnakeFilter_Test.java
@@ -2,8 +2,17 @@ package quimp.plugin;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -60,10 +69,51 @@ public class SetHeadSnakeFilter_Test {
   @Before
   public void setUp() throws Exception {
     testobj = new SetHeadSnakeFilter_();
-    d1 = new DataLoader("src/test/resources/test1.txt");
-    d2 = new DataLoader("src/test/resources/test2.txt");
-    d3 = new DataLoader("src/test/resources/test3.txt");
+    d1 = new DataLoader(loadResource(getClass().getClassLoader(), "test1.txt").toString());
+    d2 = new DataLoader(loadResource(getClass().getClassLoader(), "test2.txt").toString());
+    d3 = new DataLoader(loadResource(getClass().getClassLoader(), "test3.txt").toString());
     BOA_.qState = new BOAState();
+  }
+
+  /**
+   * Load resource file from either jar or filesystem.
+   * 
+   * <p>If class loader is an object run from jar, this method will make binary copy of resource in
+   * temporary folder and return path to it.
+   * 
+   * <p>This code is taken from
+   * https://stackoverflow.com/questions/941754/how-to-get-a-path-to-a-resource-in-a-java-jar-file
+   * 
+   * @param c class loader
+   * @param resource resource name and relative path
+   * @return path to resource file
+   */
+  public static Path loadResource(ClassLoader c, String resource) {
+    File file = null;
+    URL res = c.getResource(resource);
+    if (res.toString().startsWith("jar:")) {
+      try {
+        InputStream input = c.getResourceAsStream(resource);
+        file = File.createTempFile(new Date().getTime() + "", "");
+        OutputStream out = new FileOutputStream(file);
+        int read;
+        byte[] bytes = new byte[1024];
+
+        while ((read = input.read(bytes)) != -1) {
+          out.write(bytes, 0, read);
+        }
+        out.flush();
+        out.close();
+        input.close();
+        file.deleteOnExit();
+        return file.toPath();
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
+    } else {
+      // this will probably work in your IDE, but not from a JAR
+      return Paths.get(res.getFile());
+    }
   }
 
   /**
